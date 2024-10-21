de86edc7a6533a243c9050dec9977ab7665bdb88
cardozclive <cardozclive@gmail.com>
2024-10-21 14:09:34 +0530

    Testing

--- cypress/Integration/examples/Test11.js
+++ cypress/Integration/examples/Test11.js
@@ -6,6 +6,7 @@ describe('My eleventh test suite', () => {
         //runs once before all tests in the block
         cy.fixture('example').then(function(data){
             this.user = data
+            
         })
     })
 
