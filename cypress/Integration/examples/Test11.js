34466a0bf0d81d5cbe06bddab9a36781ec462714
cardozclive <cardozclive@gmail.com>
2024-10-22 11:30:01 +0530

    22nd Oct

--- cypress/Integration/examples/Test11.js
+++ cypress/Integration/examples/Test11.js
@@ -2,24 +2,33 @@
 
 describe('My eleventh test suite', () => {
 
-    before('Data',function(){
+    before('Data', function () {
         //runs once before all tests in the block
-        cy.fixture('example').then(function(data){
-            this.user = data
-            
+        cy.fixture('example').then(function (data) {
+            this.data = data
         })
     })
 
-    it('Hooks', function() {
+    it('Hooks', function () {
 
-        cy.visit(this.user.url)
-        cy.get('[name="name"]').eq(0).type(this.user.name)
-        cy.get('[name="email"]').type(this.user.email)
-        cy.get('#exampleInputPassword1').type(this.user.password)
+        cy.visit(this.data.url)
+        cy.get('[name="name"]').eq(0).type(this.data.name)
+        cy.get('[name="email"]').type(this.data.email)
+        cy.get('#exampleInputPassword1').type(this.data.password)
         cy.get('#exampleCheck1').check()
-        cy.get('#exampleFormControlSelect1').select(this.user.gender)
+        cy.get('#exampleFormControlSelect1').select(this.data.gender)
+        cy.pause()
         cy.get('#inlineRadio2').click()
-        // cy.get('[href*="shop"]').click()
+        cy.get('.ng-untouched').should('have.value', this.data.name)
+        cy.get(':nth-child(1) > .form-control').should('have.attr', 'minlength', '2')
+        cy.get('#inlineRadio3').should('be.disabled')
+        cy.get('[href*="shop"]').click()
+
+
+        this.data.productName.forEach(function (element) {
+            cy.selectProduct(element)
+        });
+
 
 
 
