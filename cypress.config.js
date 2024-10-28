ef0cfb6dbe568cbba2cb16eaede08a80d039a180
cardozclive <cardozclive@gmail.com>
2024-10-28 10:24:12 +0530

    28/10/2024

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,35 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    url: "https://rahulshettyacademy.com"
+  },
+  projectId: "su69fi",
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'TestReportCypressLearning',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    videoOnFailOnly:false, //<<<----------Keep it as false to record all test run videos 
+  },
+
+  video: true,
+  retries: {
+    runMode: 1,
+    openMode: 0
+  },
   defaultCommandTimeout: 4000,
   projectId: 'su69fi',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     specPattern: "cypress/integration/examples/*.js",
-    fixturesFolder: "cypress/fixtures"
-
-    
+    fixturesFolder: "cypress/fixtures",
   },
+
+  
 });
--- cypress/Integration/examples/Test11.js
+++ cypress/Integration/examples/Test11.js
@@ -17,7 +17,7 @@ describe('My eleventh test suite', () => {
         const productPage = new ProductPage()
 
         //homepage
-        cy.visit(this.data.url)
+        cy.visit(Cypress.env('url')+"/angularpractice")
         homePage.getName().type(this.data.name)
         homePage.getTwoWayDataBindin().should('have.value', this.data.name)
         homePage.getEmail().type(this.data.email)
@@ -38,35 +38,29 @@ describe('My eleventh test suite', () => {
         });
 
         productPage.checkOutBtn().click()
-        
-        // Assertion
+
+        // Adding values of all the products added to cart
         var sum = 0
-        cy.get('tr td:nth-child(4) strong').each(($el, list, index) =>{
-        //    cy.log($el.text()) 
-           var amount = $el.text().split(" ")[1].trim()
-           cy.log(amount)
-            sum = Number(sum)+Number(amount)      
-        }).then(function(){
+        cy.get('tr td:nth-child(4) strong').each(($el, list, index) => {
+
+            var amount = $el.text().split(" ")[1].trim()
+            cy.log(amount)
+            sum = Number(sum) + Number(amount)
+        }).then(function () {
             cy.log('total sum ' + sum)
         })
 
-        
-        cy.get('td h3 strong').then(function(finalAmtTxt){
+        // Assertion - Check if the total of all the amount calculated above matches with total amount displayed on the website 
+        cy.get('td h3 strong').then(function (finalAmtTxt) {
             var finalAmount = parseInt(finalAmtTxt.text().split(" ")[1].trim())
             cy.log(finalAmount)
             expect(finalAmount).to.equal(sum)
-            if(finalAmount === sum)
-                {
-                    cy.log("Sum of total of all products matches")
-                } else{
-                    cy.log("Sum of total of all products doesn't match")
-                }
-                
+            if (finalAmount === sum) {
+                cy.log("Sum of total of all products matches")
+            } else {
+                cy.log("Sum of total of all products doesn't match")
+            }
         })
-        
-
-
-
 
         productPage.clickCheckOut().click()
         productPage.typeCountryName().type('India')
@@ -81,8 +75,7 @@ describe('My eleventh test suite', () => {
         // cy.get('.alert').should('have.text',"Success! Thank you! Your order will be delivered in next few weeks :-).")
         cy.get('.alert').then(function (element) {
             const actualText = element.text()
-           expect(actualText.includes('Success!')).to.be.true
-
+            expect(actualText.includes('Success!')).to.be.true
         })
         // productPage.alertMsg().invoke('text').should('contain', "Success! Thank you! Your order will be delivered in next few weeks :-).")
     })
